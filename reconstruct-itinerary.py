49e53472c9e729b9b43ba4d70c9cdc93208661fc
Lakshaye Vaikunth <lakshayevaikunthg@gmail.com>
2024-03-01 23:41:53 -0500

    Update reconstruct-itinerary.py

--- reconstruct-itinerary.py
+++ reconstruct-itinerary.py
@@ -69,21 +69,23 @@ class Solution:
 
 # Reverse Sorted Stack-Based Approach
 
-def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-    flights = defaultdict(list)
-    for t in sorted(tickets, reverse=True):
-        flights[t[0]].append(t[1])
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        flights = defaultdict(list)
+        for t in sorted(tickets, reverse=True):
+            flights[t[0]].append(t[1])
 
-    stack, result = [], []
-    stop = "JFK"
-    while stop:
-        if not flights[stop]:
-            result.append(stop)
-            stop = None if not stack else stack.pop()
-        else:
-            stack.append(stop)
-            stop = flights[stop].pop()
-    return result[::-1]
+        stack, result = [], []
+        stop = "JFK"
+        while stop:
+            if not flights[stop]:
+                result.append(stop)
+                stop = None if not stack else stack.pop()
+            else:
+                stack.append(stop)
+                stop = flights[stop].pop()
+        return result[::-1]
+        
 
 
 
